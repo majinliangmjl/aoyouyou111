95b035fe6e854c183c88be464b4944f30f428248
majinliang <majinliang.0516@qq.com>
2017-04-19 10:36:43 +0800

    Signed-off-by: majinliang <majinliang.0516@qq.com>项目启动4/19

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name aoyouyouApp
+ * @description
+ * # aoyouyouApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('aoyouyouApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name aoyouyouApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the aoyouyouApp
+ */
+angular.module('aoyouyouApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
